0684167418c41bfc871ad12153058dca670a61cc
Moontonight1021 <81334293+Moontonight1021@users.noreply.github.com>
2021-12-27 12:57:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,23 @@ st.write(pd.DataFrame({
 
 import numpy as np
 
+#random data table
 dataframe = np.random.randn(10,20)
 st.dataframe(dataframe)
 
+
+#Plot a chart
 chart_data = pd.DataFrame(
     np.random.randn(20,3),
     columns=['a','b','c']
 )
 
 st.line_chart(chart_data)
+
+#Plot a map
+map_data = pd.DataFrame(
+    np.random.randn(1000,2) / [50,50] + [37.76, -122.4],
+    columns=['lat','lon']
+)
+
+st.map(map_data)
